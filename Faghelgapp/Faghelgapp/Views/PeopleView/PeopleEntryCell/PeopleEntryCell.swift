a4d161d9a2572aaeae84b956540a152b18b9ea56
ToreB <toreb@mesan.no>
2016-10-03 19:23:34 +0200

    Added tableviewcells for PeopleView

---
+++ Faghelgapp/Faghelgapp/Views/PeopleView/PeopleEntryCell/PeopleEntryCell.swift
@@ -0,0 +1,18 @@
+//
+//  PeopleEntryCell.swift
+//  Faghelgapp
+//
+//  Created by Tore Brandtzæg on 03.10.2016.
+//  Copyright © 2016 Idar Vassdal. All rights reserved.
+//
+
+import UIKit
+import NibDesignable
+
+class PeopleEntryCell: NibDesignableTableViewCell {
+    
+    @IBOutlet weak var personImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+}
---
+++ Faghelgapp/Faghelgapp/Views/PeopleView/PeopleHeaderCell/PeopleHeaderCell.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleEntryHeaderCellTableViewCell.swift
+//  Faghelgapp
+//
+//  Created by Tore Brandtzæg on 03.10.2016.
+//  Copyright © 2016 Idar Vassdal. All rights reserved.
+//
+
+import UIKit
+import NibDesignable
+
+class PeopleHeaderCell: NibDesignableTableViewCell {
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+}
