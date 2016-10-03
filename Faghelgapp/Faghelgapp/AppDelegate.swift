9e3a8a9aed2b912887d7150921b471cb8451635b
Anders Ullnæss <andersu@mesan.no>
2016-10-03 20:46:58 +0200

    Added icons for tabs

--- Faghelgapp/Faghelgapp/AppDelegate.swift
+++ Faghelgapp/Faghelgapp/AppDelegate.swift
@@ -10,14 +10,13 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         application.statusBarStyle = .lightContent
+        
+        UITabBar.appearance().tintColor = Constants.Colours.mesanRed
         return true
     }
-
 }
 
