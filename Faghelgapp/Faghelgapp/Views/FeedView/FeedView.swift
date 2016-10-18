a7d98767484c1857e6fff4f0419a360a7386d971
Anders Ullnæss <andersu@mesan.no>
2016-10-18 20:27:27 +0200

    Added feed to storyboard

---
+++ Faghelgapp/Faghelgapp/Views/FeedView/FeedView.swift
@@ -0,0 +1,13 @@
+//
+//  FeedView.swift
+//  Faghelgapp
+//
+//  Created by Anders Ullnæss on 18/10/16.
+//  Copyright © 2016 Idar Vassdal. All rights reserved.
+//
+
+import Foundation
+
+class FeedView: NibLoadingView {
+    
+}
