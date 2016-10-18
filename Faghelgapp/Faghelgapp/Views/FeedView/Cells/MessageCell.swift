0de71836f7778f6488c1764226842fc88d37d8e7
Anders Ullnæss <andersu@mesan.no>
2016-10-18 21:08:26 +0200

    Implemented GUI for feed

---
+++ Faghelgapp/Faghelgapp/Views/FeedView/Cells/MessageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedCell.swift
+//  Faghelgapp
+//
+//  Created by Anders Ullnæss on 18/10/16.
+//  Copyright © 2016 Idar Vassdal. All rights reserved.
+//
+
+import Foundation
+import NibDesignable
+
+class MessageCell: NibDesignableTableViewCell {
+    
+}
--- Faghelgapp/Faghelgapp/Views/FeedView/FeedView.swift
+++ Faghelgapp/Faghelgapp/Views/FeedView/FeedView.swift
@@ -10,4 +10,34 @@ import Foundation
 
 class FeedView: NibLoadingView {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    let messageCellIdentifier = "MessageCell"
+    
+    override func awakeFromNib() {
+        initTableView()
+    }
+    
+    private func initTableView() {
+        tableView.register(MessageCell.self, forCellReuseIdentifier: messageCellIdentifier)
+        
+        tableView.estimatedRowHeight = 50
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.alwaysBounceVertical = false;
+    }
+}
+
+extension FeedView: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: messageCellIdentifier, for: indexPath) as! MessageCell
+        return cell
+    }
 }
