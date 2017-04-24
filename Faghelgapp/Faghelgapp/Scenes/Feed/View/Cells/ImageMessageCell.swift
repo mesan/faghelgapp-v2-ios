18bab0a9fa216a0d60ad7f9a78c0b1200d3153dc
Tore Brandtzæg <toreb27@gmail.com>
2017-04-24 20:08:14 +0200

    Fixed some constraints in ImageMessageCell.

--- Faghelgapp/Faghelgapp/Scenes/Feed/View/Cells/ImageMessageCell.swift
+++ Faghelgapp/Faghelgapp/Scenes/Feed/View/Cells/ImageMessageCell.swift
@@ -12,27 +12,22 @@ import Kingfisher
 
 class ImageMessageCell: TextMessageCell {
     @IBOutlet weak var backgroundMessageImageView: UIImageView!
-    @IBOutlet weak var backgroundMessageImageViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var messageImageView: UIImageView!
-    @IBOutlet weak var blurHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageContainerViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageContainerView: UIView!
     
     override func populate(message: Message) {
         super.populate(message: message)
         
         if let imageUrl = message.imageUrl, let url = URL(string: imageUrl) {
-            messageImageView.isHidden = false
-            blurHeightConstraint.constant = 200
+            imageContainerView.isHidden = false
+            imageContainerViewHeightConstraint.constant = 200
             messageImageView.kf.setImage(with: ImageResource(downloadURL: url, cacheKey: imageUrl), placeholder: UIImage(named: "background_placeholder"), options: nil)
             
-            backgroundMessageImageView.isHidden = false
-            backgroundMessageImageViewHeightConstraint.constant = 200
             backgroundMessageImageView.kf.setImage(with: ImageResource(downloadURL: url, cacheKey: imageUrl), placeholder: UIImage(named: "background_placeholder"), options: nil)
         } else {
-            backgroundMessageImageView.isHidden = true
-            backgroundMessageImageViewHeightConstraint.constant = 0
-            
-            messageImageView.isHidden = true
-            blurHeightConstraint.constant = 0
+            imageContainerView.isHidden = true
+            imageContainerViewHeightConstraint.constant = 0
         }
     }
 }
